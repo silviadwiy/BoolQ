89ab51306d41a141ad0d94c3b21dc254a6e6f333
silviadwiy <117156657+silviadwiy@users.noreply.github.com>
2023-03-08 13:57:37 +0700

    Update main.py

--- challenge_data/challenge_1/main.py
+++ challenge_data/challenge_1/main.py
@@ -1,18 +1,33 @@
 import pandas as pd
-
+# from sklearn.metrics import accuracy_score
 
 def evaluate(test_annotation_file, user_submission_file, phase_codename, **kwargs):
     print("Starting Evaluation.....")
     print("Submission related metadata:")
+
+    # Load the CSV files into pandas dataframes
+    df_true = pd.read_csv(test_annotation_file)
+    df_pred = pd.read_csv(user_submission_file)
+
+    # Remove missing rows and duplicates
+    df_true = df_true.dropna().drop_duplicates()
+    df_pred = df_pred.dropna().drop_duplicates()
+
+    # Merge the dataframes on the title column
+    df = pd.merge(df_true, df_pred, on='title')
+
+    # Extract the boolean answers from each dataframe
+    correct_answers = df_true["true_answer"].values.tolist()
+    student_answers = df_pred["answer"].values.tolist()
+
+    # Compare student's answers to correct answers and calculate accuracy
+    num_correct = 0
+    for i in range(len(correct_answers)):
+        if student_answers[i] == correct_answers[i]:
+            num_correct += 1
+
+    acc = num_correct / len(correct_answers)
     
-    ground_truth = pd.read_csv(test_annotation_file)
-    prediction = pd.read_csv(user_submission_file)
-    
-    merged_df22 = pd.merge(ground_truth, prediction, on=["title", "answer"])
-    correct_predictions = merged_df22.shape[0]
-    
-    total_predictions = ground_truth.shape[0]
-    accuracy = correct_predictions / total_predictions
     print(kwargs["submission_metadata"])
     output = {}
     if phase_codename == "train":
@@ -20,7 +35,7 @@ def evaluate(test_annotation_file, user_submission_file, phase_codename, **kwarg
         output["result"] = [
             {
                 "train_split": {
-                    "Score": accuracy,
+                    "Score": acc,
                 }
             }
         ]
@@ -32,7 +47,7 @@ def evaluate(test_annotation_file, user_submission_file, phase_codename, **kwarg
         output["result"] = [
             {
                 "dev_split": {
-                    "Score": accuracy,
+                    "Score": acc,
                 }
             }
         ]
